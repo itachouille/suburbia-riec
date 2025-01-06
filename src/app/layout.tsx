8ea9848fb29386ccd8587f228fedf6a7a0cfea1b
Anthony David <anthony.david34@sfr.fr>
2025-01-06 22:30:46 +0100

    products grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 import "./globals.css";
+
 import { Header } from "@/components/Header";
 import { SVGFilters } from "@/components/SVGFilters";
 
